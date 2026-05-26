762b2b37a9c7bcc8e99546593fb6593d1f3b58ea
Markus Koschany <apo@debian.org>
2026-05-25 19:10:28 -0700

    Add Debian do_not_overrun_display_name_variable.patch
    https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=919589
    libkxl0: Buffer-overflow with potential security issue
    Reported: Emmanuel Fleury <emmanuel.fleury@u-bordeaux.fr>
    Bugfix: Bernhard Übelacker <bernhardu@mailbox.org>
    Uploader: Markus Koschany <apo@debian.org>  Fri, 18 Jan 2019

--- src/KXLvisual.c
+++ src/KXLvisual.c
@@ -296,7 +296,8 @@ void KXL_Put_StretchImage(KXL_Image *img, Uint16 src_l, Uint16 src_t, Uint16 src
 //==============================================================
 void KXL_DisplayName(const char *name)
 {
-  snprintf(KXL_DName, 1024, "%s", name);
+  snprintf(KXL_DName, sizeof(KXL_DName), "%s", name);
+  KXL_DName[sizeof(KXL_DName)-1] = '\0';
 }
 
 //==============================================================
